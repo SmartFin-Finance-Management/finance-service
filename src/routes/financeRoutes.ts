09ae9ed9a3c38eeb12bd0a1051a93efff07a5bb3
Visweshvar-UST <287618@ust.com>
2024-10-29 14:44:57 +0530

    initial

--- src/routes/financeRoutes.ts
+++ src/routes/financeRoutes.ts
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/finance', getAllFinance); 
 router.get('/finance/:id', getFinanceById); 
-router.post('/finance', addFinance); 
+router.post('/finance', addFinance);
 router.put('/finance/:id', updateFinance);  
 router.delete('/finance/:id', deleteFinance); 
 
